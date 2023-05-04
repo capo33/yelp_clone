f6d8628d7fa644336b9d586047049e19ed1c37b1
Mohamed <s2000588@edu.bc.fi>
2023-05-05 00:46:59 +0300

    add build folder to frontend and add middleware to the backend

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 
 import restaurantRoutes from "./routes/restaurant.routes";
+import path from "path";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -14,10 +15,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware & Enable CORS
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 // Welcome route
 app.get("/", (req, res) => {
   res.json({
@@ -25,11 +26,14 @@ app.get("/", (req, res) => {
   });
 });
 
-
 // Routes
 app.use("/api/v1/restaurants", restaurantRoutes);
 
 // Listen on port
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
